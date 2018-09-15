7845dc256475ef84556717fec06d3876fd7818c1
Travis T <travistokuyama@gmail.com>
2018-09-15 19:17:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   key = ""
-  min_value = Integer
+  min_value = 0
   name_hash.each do |name, value|
-    if value < min_value
+    if value <= min_value
       min_value = value
       key = name
     end
